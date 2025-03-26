ea954138fa7e1674a9878fe3a04f87789bb7095f
Pablo Schmidt <pablogschmidt2002@yahoo.com.ar>
2025-03-26 23:31:15 +0100

    Animation walking in all directions and added game mode
    Animation walking in all directions and added game mode for travel seamesly, no disconecting from the server

--- Blaster/Source/Blaster/Character/BlasterCharacter.cpp
+++ Blaster/Source/Blaster/Character/BlasterCharacter.cpp
@@ -4,6 +4,7 @@
 #include "BlasterCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 ABlasterCharacter::ABlasterCharacter()
 {
@@ -17,6 +18,10 @@ ABlasterCharacter::ABlasterCharacter()
     FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
     FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
     FollowCamera->bUsePawnControlRotation = false;
+    // next line is to avoid the player rotating with the controller rotation
+    bUseControllerRotationYaw = false;
+    //
+    GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 void ABlasterCharacter::BeginPlay()
